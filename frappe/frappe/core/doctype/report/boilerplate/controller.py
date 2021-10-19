c36da91d8958a059fd12196b6e8ccd69223d5caa
shreyas <you@example.com>
2021-10-19 19:13:24 +0530

    My test file

--- frappe/frappe/core/doctype/report/boilerplate/controller.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2013, {app_publisher} and contributors
-# For license information, please see license.txt
-
-# import frappe
-
-def execute(filters=None):
-	columns, data = [], []
-	return columns, data
--- frappe/frappe/query_builder/__init__.py
+++
@@ -1,2 +0,0 @@
-from pypika import *
-from frappe.query_builder.utils import get_query_builder, patch_query_execute
